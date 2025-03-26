dde7679b131adc8397481b3fcfb5bc05b461089b
Landon OHare <lmohare@mail.fhsu.edu>
2025-03-26 16:06:33 -0500

    Changed connection in database

--- config/Database.php
+++ config/Database.php
@@ -21,7 +21,7 @@ class Database {
         try {
             // Use pgsql:host=... instead of mysql:host=...
             // Also include port=5432 explicitly if needed
-            $dsn = "pgsql:host={$this->host};port=5432;dbname={$this->db_name}";
+            $dsn = "pgsql:host={$this->host};port=5432;dbname={$this->db_name};sslmode=require";
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $exception) {
